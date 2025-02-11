3ca8313da16170fa6dc351ba713790fc59882403
Do_Van_Minh <dominh03122003@gmail.com>
2025-02-11 12:36:01 +0700

    da sua ben dominh

---
+++ src/libs/api/personnel.api.ts
@@ -0,0 +1,30 @@
+import { GetPersonnel_DTO, Personnel_DTO } from '../../models/personnel.model';
+import { CallApi } from '@/libs/call_API';
+
+export const PersonnelAPI = {
+  getPersonnelsByPageOrder: async (
+    pageIndex: number,
+    pageSize: number,
+    orderType: 'ASC' | 'DESC',
+  ) => {
+    const data: GetPersonnel_DTO[] = await CallApi.getAll<GetPersonnel_DTO>(
+      `personnel?pageIndex=${pageIndex}&pageSize=${pageSize}&orderType=${orderType}`,
+    );
+    return data;
+  },
+
+  createPersonnel: async (newPersonnel: Personnel_DTO) => {
+    const data = await CallApi.create<number>('personnel', newPersonnel);
+    return data;
+  },
+
+  updatePersonnel: async (personnel: Personnel_DTO) => {
+    const data = await CallApi.update<number>('personnel', personnel);
+    return data;
+  },
+
+  deletePersonnel: async (Id: number): Promise<number> => {
+    const data = await CallApi.delete<number>('personnel', Id);
+    return data;
+  },
+};
